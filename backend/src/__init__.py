d92043aabdeaa2cd68aa5af845f59dd7901602c0
SukritTM <sukrittmani@gmail.com>
2021-05-11 16:44:36 +0530

    created app

---
+++ backend/src/__init__.py
@@ -0,0 +1,31 @@
+import os 
+from flask import Flask
+
+def create_app(test_config=None):
+    # create the app
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'src.sqlite')
+    )
+
+    if test_config is None:
+        # load from instance file if exists
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load from test config when testing
+        app.config.from_mapping(test_config)
+
+    # ensure instance directory exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # test page
+    @app.route('/hello')
+    def hello():
+        return 'hello world'
+    
+    return app
